ae8a9c691d3cf1f21d065ec47c0c1d7789240a11
Kevin Amiranoff <kamiranoff@gmail.com>
2018-11-15 00:01:13 +0000

    Adds tags and cloud spawner/collider

---
+++ Assets/Scripts/Cloud Scripts/CloudSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+
+
+	[SerializeField] private GameObject[] clouds;
+	[SerializeField] private GameObject[] collectables;
+
+	private float _distanceBetweenClouds = 3f;
+
+	private float _minX;
+	private float _maxX;
+
+	private float _lastCloudPositionY;
+	private float _controlX;
+
+	private GameObject _player;
+
+
+	void SetMinAndMaxX()
+	{
+		
+	}
+
+
+}
